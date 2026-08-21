9247943f7418faa9c2a71e59e422c2257a1066c5
Lizay Khan <lizaykhan5@gmail.com>
2026-08-21 11:07:23 +0500

    Fix Neon database connection

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -164,24 +164,20 @@ else:
 # =========================================================
 # DATABASE — POSTGRESQL
 # =========================================================
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config(
-            "DB_HOST",
-            default="localhost",
-        ),
-        "PORT": config(
-            "DB_PORT",
-            default="5432",
-        ),
-        "CONN_MAX_AGE": 600,
+        "NAME": config("DB_NAME").strip(),
+        "USER": config("DB_USER").strip(),
+        "PASSWORD": config("DB_PASSWORD").strip(),
+        "HOST": config("DB_HOST").strip(),
+        "PORT": config("DB_PORT", default="5432").strip(),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
+
 
 
 # =========================================================
